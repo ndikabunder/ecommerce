5f9b7dc7a32f66e41f9b8f9a7b05aa2559a48c83
ndikabunder <ndikabunder@gmail.com>
2022-10-08 08:20:04 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        serif: ['Lora', 'serif'],
+      },
+      colors: {
+        primary: '#413DEE;',
+      },
+    },
+  },
+  plugins: [],
+};
